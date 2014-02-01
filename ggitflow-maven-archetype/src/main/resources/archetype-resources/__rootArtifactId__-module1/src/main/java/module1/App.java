a747d6abf5cd1302e66cc76b31dc9e42955fe07f
Desmond Kirrane <desmond.kirrane@gmail.com>
2014-02-01 16:47:41 +0000

    Multi module pom fixes

--- ggitflow-maven-archetype/src/main/resources/archetype-resources/__rootArtifactId__-module1/src/main/java/module1/App.java
+++ ggitflow-maven-archetype/src/main/resources/archetype-resources/__rootArtifactId__-module1/src/main/java/module1/App.java
@@ -1,17 +1,11 @@
  #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-package $
-
-{package
-
-
-
-}.module1;
+package ${package}.module1;
 
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Module1!");
     }
 }
--- ggitflow-maven-archetype/src/main/resources/archetype-resources/__rootArtifactId__-module2/src/test/java/module2/AppTest.java
+++ ggitflow-maven-archetype/src/main/resources/archetype-resources/__rootArtifactId__-module2/src/test/java/module2/AppTest.java
@@ -1,17 +1,13 @@
  #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-package $
-
-{package}.module2;
+package ${package}.module2;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-
 
 public class AppTest {
 
